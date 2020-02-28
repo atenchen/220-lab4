500d931ccd217133ae259423f060578ed1a069cb
Alvin Chen <achen172@binghamton.edu>
2020-02-28 13:48:50 -0500

    lab 4

---
+++ chess.c
@@ -0,0 +1,48 @@
+/* chess.c */
+#include <stdint.h>
+#include <stdio.h>
+#include "chess.h"
+
+
+uint64_t king_pos = 0;
+
+unsigned int make_move(Move m){
+  if(!IS_SET(king_pos,m.from)){
+    return 0;
+  }
+  if(m.to != NORTH_OF(m.from) && m.to != SOUTH_OF(m.from) && m.to != EAST_OF(m.from) && m.to != WEST_OF(m.from) && m.to != SE_OF(m.from) && m.to != SW_OF(m.from) && m.to != NE_OF(m.from) && m.to != NW_OF(m.from)){
+    return 0;
+  }
+  CLEAR_BIT(king_pos, m.from);
+  SET_BIT(king_pos, m.to);
+  return 1;
+}
+
+int main(){
+  char s[128];
+  uint8_t index;
+  unsigned int error;
+  Move m = {INVALID_INDEX, INVALID_INDEX};
+
+  printf("Enter initial king position: ");
+  fgets(s, 127, stdin);
+  index = notation_to_idx(s[0], s[1]);
+  if(index == INVALID_INDEX){
+    printf("error");
+    exit(0);
+  }
+  SET_BIT(king_pos, index);
+  
+  display_board();
+
+  for(; ;){
+    m = get_move();
+    if(make_move(m) != 1){
+      printf("Invalid move. Try again!\n");
+    }
+    else{
+      display_board();
+    }
+  }
+  return 0;
+}
---
+++ io.c
@@ -0,0 +1,71 @@
+#include <stdint.h>
+#include <stdio.h>
+#include "chess.h"
+
+
+uint8_t notation_to_idx(char file, char rank){
+  if(file < 'a' || file > 'h' || rank > '8' || rank < '1'){
+    return INVALID_INDEX;
+  }
+  uint8_t Id = (8 - (rank - '0')) * 8 + file - 'a';
+  printf("%d\n", Id);
+  return Id;
+}
+
+char get_piece_at(uint8_t i){
+  if(IS_SET(king_pos, i)){
+    return 'K';
+  }
+  return ' ';
+}
+
+void display_board(){
+  unsigned int i = 0, j = 0;
+  printf("\n −−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−\n");
+  for ( i = 0 ; i < 8 ; i++) { /* Rank */
+    printf("%d |", 8 - i);
+    for(j = 0; j < 8; j++){
+      printf(" %c|", get_piece_at(i*8+j));
+    }
+    printf("\n −−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−\n");
+  }
+  printf( "   a  b  c  d  e  f  g  h\n");
+  printf("\n");
+}
+
+Move get_move(){
+  char s[128];
+  unsigned int error;
+  Move m = {INVALID_INDEX, INVALID_INDEX};
+
+  error = 0;
+  do {
+    if(error == 1){
+      printf("Try again. \n");
+    }
+    error = 0;
+    printf("Enter move: ");
+    fgets(s, 127, stdin);
+    s[5] = '\0';
+    /*printf("s[0] = %c, s[1] = %c, s[2] = %c s[3] = %c, s[4] = %c\n", s[0], s[1], s[2], s[3], s[4]);
+     */
+    m.from = notation_to_idx(s[0], s[1]);
+    if(m.from == INVALID_INDEX){
+      printf("Invalid from. \n");
+      error = 1;
+      continue;
+    }
+    if(s[2] != '-' ){
+      printf("A '-' expected after from square. \n");
+      error = 1;
+      continue;
+    }
+    m.to = notation_to_idx(s[3], s[4]);
+    if(m.to == INVALID_INDEX){
+      printf("Invalid to. \n");
+      error = 1;
+      continue;
+    }
+  } while (error == 1);
+  return m;
+}
